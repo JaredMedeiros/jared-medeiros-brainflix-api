425123f89c23d26118f1f8f54c3ca3dd895e19b9
Jared Medeiros <jared14jared@gmail.com>
2022-03-10 09:52:35 -0500

    set up server port, and created a skeleton for the routes to read and write json data

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-console.log('hello world')
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app,use(express.static('./public'))
+
+app.use((req, res, next) => {
+    console.log(`Incoming request for path ${req.path}`);
+    next();
+});
+
+app.use((req, res, next) => {
+    if (
+        req.method === 'POST' &&
+        req.headers['content-type'] !== 'application/json'
+    ) {
+        return res.status(400).json()({
+            message: 'POST requests must contani content-type=application/json',
+        });
+    } else {
+        next();
+    }
+});
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(/*api route*/, videoRoutes)
+
+const SERVER_PORT = 5500;
+
+app.listen(SEREVER_PORT, () => {
+    console.log(`Server is listening on port ${SERVER_PORT}`);
+});
