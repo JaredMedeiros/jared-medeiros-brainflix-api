2f467951fc08d1041f41341be8ee441072a3043a
Jared Medeiros <jared14jared@gmail.com>
2022-03-12 14:26:30 -0500

    server.js set up

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-app,use(express.static('./public'))
+app.use(express.static('./public'))
 
 app.use((req, res, next) => {
     console.log(`Incoming request for path ${req.path}`);
